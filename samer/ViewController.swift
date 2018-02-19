43b594ef4593979d13dacb2968c55de8a56bf5ba
amryousry <amryousry94@hotmail.com>
2018-02-19 23:07:14 +0200

    Initial Commit

---
+++ samer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  samer
+//
+//  Created by amr yousry on 2/19/18.
+//  Copyright © 2018 amr yousry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
